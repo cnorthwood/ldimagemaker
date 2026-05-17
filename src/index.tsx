2ed50f05c0b15fc1cac07832fb606beef882f93e
Dan Walker <daniel.walker@ncas.ac.uk>
2026-05-17 16:26:48 +0100

    Actually displaying in node 24

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
 import "./styles.scss";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container); // 1. Create root
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root"),
-);
+); // 2. Render app
